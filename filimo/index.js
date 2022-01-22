a17773b33a28b4fb6e6a96141a1d2cd0d5174b1e
ehsan <ehsan.haqa@gmail.com>
2022-01-22 13:12:58 +0330

    filimo

---
+++ filimo/index.js
@@ -0,0 +1,119 @@
+let filimo = [
+    {
+        banner : "" ,
+        rate : "83%" ,
+        name : "نیسان ابی" ,
+        season : "1" ,
+        episode : "14" ,
+        yaer : "1400" ,
+        genre : "کمدی و خانوادگی" 
+    } , 
+    {
+        banner : "" ,
+        rate : "78%" ,
+        name : "پوست" ,
+        yaer : "1397" ,
+        genre : "اجتماعی و خانوادگی" 
+    } ,
+    {
+        banner : "" ,
+        rate : "71%" ,
+        name : "وارهانت" ,
+        yaer : "2022" ,
+        genre : "اکشن و هیجان انگیز" 
+    } ,
+    {
+        banner : "" ,
+        rate : "92%" ,
+        name : "پایگاه مرزی" ,
+        season : "1" ,
+        episode : "1" ,
+        yaer : "2018" ,
+        genre : "اکشن و علمی تخیلی" 
+    } ,
+    {
+        banner : "" ,
+        rate : "97%" ,
+        name : "جوکر" ,
+        season : "2" ,
+        episode : "4" ,
+        yaer : "1400" ,
+        genre : "کمدی و خانوادگی" 
+    } , 
+    {
+        banner : "" ,
+        rate : "79%" ,
+        name : "خدمتکار" ,
+        season : "3" ,
+        episode : "1" ,
+        yaer : "2021" ,
+        genre : "وحشت و هیجانگیز" 
+    } ,
+    {
+        banner : "" ,
+        rate : "86%" ,
+        name : "بالارو نگاه نکن" ,
+        yaer : "2021" ,
+        genre : "کمدی و خانوادگی" 
+    } ,
+    {
+        banner : "" ,
+        rate : "96%" ,
+        name : "ممیزی" ,
+        season : "2" ,
+        episode : "73" ,
+        yaer : "1400" ,
+        genre : "تاک شو" 
+    } ,
+    {
+        banner : "" ,
+        rate : "89%" ,
+        name : "شکارچیان روح" ,
+        yaer : "2021" ,
+        genre : "کمدی و خانوادگی" 
+    } , 
+    {
+        banner : "" ,
+        rate : "90%" ,
+        name : "جزیره" ,
+        season : "1" ,
+        episode : "13" ,
+        yaer : "1400" ,
+        genre : "اجتماعی و خانوادگی" 
+    } ,
+    {
+        banner : "" ,
+        rate : "94%" ,
+        name : "بستمین سالگرد هریپاتر" ,
+        yaer : "2022" ,
+        genre : "مستند و اجتماعی" 
+    } ,
+    {
+        banner : "" ,
+        rate : "97%" ,
+        name : "هتل ترنسیلوانیا 4" ,
+        yaer : "2021" ,
+        genre : "انیمیشن و ماجراجویی" 
+    } ,
+    {
+        banner : "" ,
+        rate : "99%" ,
+        name : "آواز 2" ,
+        yaer : "2021" ,
+        genre : "انیمیشن و ماجراجویی" 
+    } , 
+    {
+        banner : "" ,
+        rate : "94%" ,
+        name : "ماجراهای پیل" ,
+        yaer : "2021" ,
+        genre : "انیمیشن و ماجراجویی"  
+    } ,
+    {
+        banner : "" ,
+        rate : "93%" ,
+        name : "جاودانگان" ,
+        yaer : "2021" ,
+        genre : "اکشن و ماجراجویی" 
+    } 
+]
