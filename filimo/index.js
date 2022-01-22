70a3cc500b370db52e50d6579125f6ca7c9e7b9f
ehsan <ehsan.haqa@gmail.com>
2022-01-22 13:47:35 +0330

    filimo

--- filimo/index.js
+++ filimo/index.js
@@ -1,6 +1,6 @@
 let filimo = [
     {
-        banner : "" ,
+        banner : "https://static.cdn.asset.filimo.com/flmt/mov_105664_22247-m.jpg" ,
         rate : "83%" ,
         name : "نیسان ابی" ,
         season : "1" ,
@@ -9,21 +9,21 @@ let filimo = [
         genre : "کمدی و خانوادگی" 
     } , 
     {
-        banner : "" ,
+        banner : "https://static.cdn.asset.filimo.com/flmt/mov_105819_21904-m.jpg" ,
         rate : "78%" ,
         name : "پوست" ,
         yaer : "1397" ,
         genre : "اجتماعی و خانوادگی" 
     } ,
     {
-        banner : "" ,
+        banner : "https://static.cdn.asset.filimo.com/flmt/mov_106324_22443-m.jpg" ,
         rate : "71%" ,
         name : "وارهانت" ,
         yaer : "2022" ,
         genre : "اکشن و هیجان انگیز" 
     } ,
     {
-        banner : "" ,
+        banner : "https://static.cdn.asset.filimo.com/flmt/mov_38482_22246-m.jpg" ,
         rate : "92%" ,
         name : "پایگاه مرزی" ,
         season : "1" ,
@@ -32,7 +32,7 @@ let filimo = [
         genre : "اکشن و علمی تخیلی" 
     } ,
     {
-        banner : "" ,
+        banner : "https://static.cdn.asset.filimo.com/flmt/mov_105658_21803-m.jpg" ,
         rate : "97%" ,
         name : "جوکر" ,
         season : "2" ,
@@ -41,7 +41,7 @@ let filimo = [
         genre : "کمدی و خانوادگی" 
     } , 
     {
-        banner : "" ,
+        banner : "https://static.cdn.asset.filimo.com/flmt/mov_106321_22451-m.jpg" ,
         rate : "79%" ,
         name : "خدمتکار" ,
         season : "3" ,
@@ -50,14 +50,14 @@ let filimo = [
         genre : "وحشت و هیجانگیز" 
     } ,
     {
-        banner : "" ,
+        banner : "https://static.cdn.asset.filimo.com/flmt/mov_92943_19151-m.jpg" ,
         rate : "86%" ,
         name : "بالارو نگاه نکن" ,
         yaer : "2021" ,
         genre : "کمدی و خانوادگی" 
     } ,
     {
-        banner : "" ,
+        banner : "https://static.cdn.asset.filimo.com/flmt/mov_105665_22421-m.jpg" ,
         rate : "96%" ,
         name : "ممیزی" ,
         season : "2" ,
@@ -66,14 +66,14 @@ let filimo = [
         genre : "تاک شو" 
     } ,
     {
-        banner : "" ,
+        banner : "https://static.cdn.asset.filimo.com/flmt/mov_93566_20830-m.jpg" ,
         rate : "89%" ,
         name : "شکارچیان روح" ,
         yaer : "2021" ,
         genre : "کمدی و خانوادگی" 
     } , 
     {
-        banner : "" ,
+        banner : "https://static.cdn.asset.filimo.com/flmt/mov_105663_21768-m.jpg" ,
         rate : "90%" ,
         name : "جزیره" ,
         season : "1" ,
@@ -82,35 +82,35 @@ let filimo = [
         genre : "اجتماعی و خانوادگی" 
     } ,
     {
-        banner : "" ,
+        banner : "https://static.cdn.asset.filimo.com/flmt/mov_104682_20287-m.jpg" ,
         rate : "94%" ,
         name : "بستمین سالگرد هریپاتر" ,
         yaer : "2022" ,
         genre : "مستند و اجتماعی" 
     } ,
     {
-        banner : "" ,
+        banner : "https://static.cdn.asset.filimo.com/flmt/mov_rel_10945_999-m.jpg" ,
         rate : "97%" ,
         name : "هتل ترنسیلوانیا 4" ,
         yaer : "2021" ,
         genre : "انیمیشن و ماجراجویی" 
     } ,
     {
-        banner : "" ,
+        banner : "https://static.cdn.asset.filimo.com/flmt/mov_94858_20878-m.jpg" ,
         rate : "99%" ,
         name : "آواز 2" ,
         yaer : "2021" ,
         genre : "انیمیشن و ماجراجویی" 
     } , 
     {
-        banner : "" ,
+        banner : "https://static.cdn.asset.filimo.com/flmt/mov_103626_19490-m.jpg" ,
         rate : "94%" ,
         name : "ماجراهای پیل" ,
         yaer : "2021" ,
         genre : "انیمیشن و ماجراجویی"  
     } ,
     {
-        banner : "" ,
+        banner : "https://static.cdn.asset.filimo.com/flmt/mov_93572_21490-m.jpg" ,
         rate : "93%" ,
         name : "جاودانگان" ,
         yaer : "2021" ,
